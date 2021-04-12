f7cb1c7e5894b0cd982cf3d9469a6ed829e6d8ef
huangchaoqun6 <1321735387@qq.com>
2021-04-12 23:22:07 +0800

    add SortAlgo.py

--- DataStruct/SortAlgo.py
+++ DataStruct/SortAlgo.py
@@ -62,6 +62,10 @@ def SelectSort(alist: list) -> list:
         1. 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置。
         2. 再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
         3. 重复第二步，直到所有元素均排序完毕
+    时间复杂度:
+        最佳情况: T(n) = O(n^2)
+        最差情况: T(n) = O(n^2)
+        平均情况: T(n) = O(n^2)
     """
     print("SelectSort: ")
     print("Source List: ", alist)
@@ -77,11 +81,41 @@ def SelectSort(alist: list) -> list:
     return alist
 
 
+# 插入排序: 内部排序, 稳定, 比较排序
+def InsertSort(alist: list) -> list:
+    """ 
+    插入排序的原理: 内部排序, 稳定, 比较排序
+        插入排序是一种最简单直观的排序算法，它的工作原理是通过构建有序序列，对于未排序数据，
+        在已排序序列中从后向前扫描，找到相应位置并插入。插入排序和冒泡排序一样，也有一种优化算法，叫做拆半插入。
+    步骤:
+        将第一待排序序列第一个元素看做一个有序序列，把第二个元素到最后一个元素当成是未排序序列。
+        从头到尾依次扫描未排序序列，将扫描到的每个元素插入有序序列的适当位置
+        如果待插入的元素与有序序列中的某个元素相等，则将待插入元素插入到相等元素的后面。
+    """
+    print("InsertSort: ")
+    print("Source List: ", alist)
+    for i in range(len(alist)):
+        pre_index = i - 1
+        current = alist[i]
+        while pre_index >= 0 and alist[pre_index] > current:
+            alist[pre_index + 1] = alist[pre_index]
+            pre_index -= 1
+        alist[pre_index + 1] = current
+        print("第{}次进行插入".format(i), alist)
+    print('InsertSort: ', alist)
+    return alist
+
+# 希尔排序: 内部排序, 不稳定
+
+
 def TestSort() -> None:
     alist = [3, 1, 9, 24, 12, 34, 56, 97, 25]
     BubbleSort(alist)
     alist = [3, 1, 9, 24, 12, 34, 56, 97, 25]
     SelectSort(alist)
+    alist = [3, 1, 9, 24, 12, 34, 56, 97, 25]
+    InsertSort(alist)
+
 
 
 if __name__ == '__main__':
